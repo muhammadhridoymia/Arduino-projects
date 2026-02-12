24f6f501a33e384824099b4356542de3e99401ae
Hridoy <hridoymuhammad037@gmail.com>
2026-02-12 21:52:47 +0600

    first commit

---
+++ LED blink/ledblinking.c
@@ -0,0 +1,17 @@
+const int ledPin = 13;
+const int buttonPin = 2;
+int buttonState = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH); // LED on
+  } else {
+    digitalWrite(ledPin, LOW);  // LED off
+  }
+}
